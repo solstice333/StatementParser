6dda595204e6c3e9e9001a08c7bb401d29e14b72
Kevin Navero <knavero@gmail.com>
2018-04-01 14:23:52 -0700

    config.py: added tests and a few typo bug fixes
    forgot some trivial stuff that were exposed by the tests..good thing
    the tests are automated...

--- sparser/config.py
+++ sparser/config.py
@@ -16,26 +16,26 @@ class Config(Mapping):
    def __len__(self):
       return len(self._conf)
 
-   def get_key(idx):
-      return self._conf[idx]['key']
+   def get_key(self, idx):
+      return self._conf[idx].get('key')
 
-   def get_value(idx):
-      return self._conf[idx]['value']
+   def get_value(self, idx):
+      return self._conf[idx].get('value')
 
-   def get_key_case_ignore(idx):
-      return self._conf[idx]['key_case_ignore']
+   def get_key_case_ignore(self, idx):
+      return self._conf[idx].get('key_case_ignore')
 
-   def get_key_case_ignore(idx):
-      return self._conf[idx]['value_case_ignore']
+   def get_value_case_ignore(self, idx):
+      return self._conf[idx].get('value_case_ignore')
 
-   def get_offset(idx):
-      return self._conf[idx]['offset']
+   def get_offset(self, idx):
+      return self._conf[idx].get('offset')
 
-   def get_regex_semantics(idx):
-      return self._conf[idx]['regex_semantics']
+   def get_regex_semantics(self, idx):
+      return self._conf[idx].get('regex_semantics')
 
-   def get_key_regex_semantics(idx):
-      return self._conf[idx]['key_regex_semantics']
+   def get_key_regex_semantics(self, idx):
+      return self._conf[idx].get('key_regex_semantics')
 
-   def get_value_regex_semantics(idx):
-      return self._conf[idx]['value_regex_semantics']
+   def get_value_regex_semantics(self, idx):
+      return self._conf[idx].get('value_regex_semantics')
--- tests/config_test.py
+++ tests/config_test.py
@@ -8,3 +8,44 @@ class ConfigTest(unittest.TestCase):
    def test_getitem(self):
       self.assertEqual(self.c[0]['key'], 'Previous Balance')
       self.assertEqual(self.c[1]['key'], 'new balance total')
+      self.assertEqual(self.c[2]['key'], 'Current Payment')
+
+   def test_get_key(self):
+      self.assertEqual(self.c.get_key(0), 'Previous Balance')
+      self.assertEqual(self.c.get_key(1), 'new balance total')
+      self.assertEqual(self.c.get_key(2), 'Current Payment')
+
+   def test_get_value(self):
+      self.assertEqual(self.c.get_value(0), r'(?:\-)?\$[\d,]+\.\d+')
+      self.assertEqual(self.c.get_value(1), r'(?:\-)?\$[\d,]+\.\d+')
+      self.assertEqual(self.c.get_value(2), r'(?:\-)?\$[\d,]+\.\d+')
+
+   def test_get_key_case_ignore(self):
+      self.assertEqual(self.c.get_key_case_ignore(0), None)
+      self.assertEqual(self.c.get_key_case_ignore(1), True)
+      self.assertEqual(self.c.get_key_case_ignore(2), None)
+
+   def test_get_value_case_ignore(self):
+      self.assertEqual(self.c.get_value_case_ignore(0), None)
+      self.assertEqual(self.c.get_value_case_ignore(1), True)
+      self.assertEqual(self.c.get_value_case_ignore(2), None)
+
+   def test_get_offset(self):
+      self.assertEqual(self.c.get_offset(0), None)
+      self.assertEqual(self.c.get_offset(1), 5)
+      self.assertEqual(self.c.get_offset(2), 10)
+
+   def test_get_regex_semantics(self):
+      self.assertEqual(self.c.get_regex_semantics(0), None)
+      self.assertEqual(self.c.get_regex_semantics(1), 'search')
+      self.assertEqual(self.c.get_regex_semantics(2), None)
+
+   def test_get_key_regex_semantics(self):
+      self.assertEqual(self.c.get_key_regex_semantics(0), None)
+      self.assertEqual(self.c.get_key_regex_semantics(1), None)
+      self.assertEqual(self.c.get_key_regex_semantics(2), 'match')
+
+   def test_get_value_regex_semantics(self):
+      self.assertEqual(self.c.get_value_regex_semantics(0), 'search')
+      self.assertEqual(self.c.get_value_regex_semantics(1), None)
+      self.assertEqual(self.c.get_value_regex_semantics(2), None)
