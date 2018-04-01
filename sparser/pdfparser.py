65b9113c680c6521f1df04ecf65e5be539200ac4
Kevin Navero <knavero@gmail.com>
2018-04-01 14:37:01 -0700

    pdfparser.py: separated stank

--- sparser/pdfparser.py
+++ sparser/pdfparser.py
@@ -50,31 +50,19 @@ class PDFParser(Sequence):
          self._lines = self.text.splitlines()
       return self._lines
 
-   def __getitem__(self, idx):
+   def _parse_through_pdf(self, key_parse_meth, val_parse_meth, offset):
       lines = self.lines
-      spec = self._conf[idx]
-      
-      parse_key = self._str_to_parse_semantics(idx, 
-         self._build_key_regex(idx), 
-         'key_regex_semantics' \
-            if 'key_regex_semantics' in spec else 'regex_semantics')
-      parse_val = self._str_to_parse_semantics(idx, 
-         self._build_value_regex(idx), 
-         'value_regex_semantics' \
-            if 'value_regex_semantics' in spec else 'regex_semantics')
-
       kv = {'key': None, 'value': None}
       Extracted = namedtuple('Extracted', ['key', 'value'])
-      offset = spec.get('offset') or 0
       offset_cnt = 0
       found_key = False
       found_val = False
 
       for line in lines:
-         if not found_key and parse_key(line):
+         if not found_key and key_parse_meth(line):
             kv['key'] = line
             found_key = True 
-         if not found_val and parse_val(line):
+         if not found_val and val_parse_meth(line):
             if offset == offset_cnt:
                kv['value'] = line
                found_val = True
@@ -83,6 +71,19 @@ class PDFParser(Sequence):
             break
 
       return Extracted(**kv)
+
+   def __getitem__(self, idx):
+      spec = self._conf[idx]
+      parse_key = self._str_to_parse_semantics(idx, 
+         self._build_key_regex(idx), 
+         'key_regex_semantics' \
+            if 'key_regex_semantics' in spec else 'regex_semantics')
+      parse_val = self._str_to_parse_semantics(idx, 
+         self._build_value_regex(idx), 
+         'value_regex_semantics' \
+            if 'value_regex_semantics' in spec else 'regex_semantics')
+      offset = spec.get('offset') or 0
+      return self._parse_through_pdf(parse_key, parse_val, offset)
 
    def __len__(self):
       return len(self._conf)
