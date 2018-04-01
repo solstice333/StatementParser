c19080f6399f7cbad0495d7d6143922e37cc10c2
Kevin Navero <knavero@gmail.com>
2018-04-01 14:07:28 -0700

    config.py: add convenience api's

--- sparser/config.py
+++ sparser/config.py
@@ -7,8 +7,8 @@ class Config(Mapping):
       with open(filepath) as conf:
          self._conf = json.load(conf)
 
-   def __getitem__(self, key):
-      return self._conf[key]
+   def __getitem__(self, idx):
+      return self._conf[idx]
 
    def __iter__(self):
       return iter(self._conf)
@@ -16,3 +16,26 @@ class Config(Mapping):
    def __len__(self):
       return len(self._conf)
 
+   def get_key(idx):
+      return self._conf[idx]['key']
+
+   def get_value(idx):
+      return self._conf[idx]['value']
+
+   def get_key_case_ignore(idx):
+      return self._conf[idx]['key_case_ignore']
+
+   def get_key_case_ignore(idx):
+      return self._conf[idx]['value_case_ignore']
+
+   def get_offset(idx):
+      return self._conf[idx]['offset']
+
+   def get_regex_semantics(idx):
+      return self._conf[idx]['regex_semantics']
+
+   def get_key_regex_semantics(idx):
+      return self._conf[idx]['key_regex_semantics']
+
+   def get_value_regex_semantics(idx):
+      return self._conf[idx]['value_regex_semantics']
